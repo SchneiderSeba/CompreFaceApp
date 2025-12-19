fe071a661717a8d39dd99ee198e77ecd4e9d89d4
SchneiderSeba <seba_19_sc@hotmail.com>
2025-12-18 21:41:55 -0300

    Deploy multi service .toml

--- FrontEnd/faceApp/src/components/WebCapture.tsx
+++ FrontEnd/faceApp/src/components/WebCapture.tsx
@@ -3,7 +3,7 @@ import './WebCapture.css';
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://comprefaceapp-production.up.railway.app';
 
 export const WebCapture = () => {
 
