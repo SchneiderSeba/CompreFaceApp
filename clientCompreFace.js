863d7ae63410fc5c3369bbb5b9ff8a3ba7436e0d
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-09 17:29:45 -0300

    ...

--- clientCompreFace.js
+++ clientCompreFace.js
@@ -1,6 +1,6 @@
 import { CompreFace } from '@exadel/compreface-js-sdk';
 
-const url = 'https://compreface.schneidersebastian.com';
-const port = 443;
+const url = process.env.COMPRE_FACE_URL || 'https://demo.compreface.ai';
+// const port = 443;
 
-export const compreFace = new CompreFace(url, port);
+export const compreFace = new CompreFace(url);
