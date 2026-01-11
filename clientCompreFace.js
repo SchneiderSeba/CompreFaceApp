d429bbca0827d5d3072fa14f3b04ddd81f6239bc
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-11 14:05:54 -0300

    ..v1

--- clientCompreFace.js
+++ clientCompreFace.js
@@ -1,8 +1,7 @@
 import { CompreFace } from '@exadel/compreface-js-sdk';
 
-const urlRaw = process.env.COMPRE_FACE_URL || 'https://compreface.schneidersebastian.com';
+const urlRaw = process.env.COMPRE_FACE_URL || 'compreface.schneidersebastian.com';
+const domain = urlRaw.replace(/^https?:\/\//, '').replace(/\/$/, '');
 
-// Eliminamos el protocolo 'https://' porque el SDK lo añade internamente según el puerto
-const cleanDomain = urlRaw.replace(/^https?:\/\//, '').replace(/\/$/, '');
-
-export const compreFace = new CompreFace(cleanDomain, 443);
+// Forzamos el puerto 443 para HTTPS (Contabo/Caddy)
+export const compreFace = new CompreFace(domain, 443);
