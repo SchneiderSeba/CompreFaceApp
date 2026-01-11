91db9bf9eca54e54b3f94176ecc4c6ed3807ffb3
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-11 13:35:00 -0300

    Update shared variables

--- FrontEnd/faceApp/src/components/AddManually.tsx
+++ FrontEnd/faceApp/src/components/AddManually.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './AddManually.css';
 import type { FaceItem } from '../types';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || "https://comprefaceapp-production-a8a0.up.railway.app";
 
 export const AddManually = () => {
 
