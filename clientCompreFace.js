3116e45172199b601a3edb9104a7ad446c488cc0
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-11 14:32:46 -0300

    my solution

--- clientCompreFace.js
+++ clientCompreFace.js
@@ -3,6 +3,7 @@ import { CompreFace } from '@exadel/compreface-js-sdk';
 // Pasamos la URL completa con HTTPS. 
 // Esto obliga al SDK a usar el puerto 443 y el protocolo correcto.
 const url = "https://compreface.schneidersebastian.com";
+const port = 8000
 
 // NO pases el segundo parámetro (puerto). Déjalo solo con la URL.
-export const compreFace = new CompreFace(url);
+export const compreFace = new CompreFace(url, port);
