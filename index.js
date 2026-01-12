b00e757a838ca4182ca51a1d7b621e7224bf83e2
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-12 12:01:12 -0300

    Helmet Lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { addNewFaceToPullManualy } from './faceRecognice.js';
+import helmet from 'helmet';
 
 dotenv.config();
 
